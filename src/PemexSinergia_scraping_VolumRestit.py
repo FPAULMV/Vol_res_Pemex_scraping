9ba744b943491814e61cc9d67ed1d39be35ff3b0
Your Name <you@example.com>
2024-04-26 17:40:39 -0600

    En esta actualizacion se agrega un proyecto nuevo donde podemos definir una fecha de inicio para que comience a hacer el rascado de la informacion.
    El proyecto donde realiza la consulta diara se encuentra terminado.

--- src/PemexSinergia_scraping_VolumRestit.py
+++ src/PemexSinergia_scraping_VolumRestit.py
@@ -1,48 +1,68 @@
-import pyodbc
+# Importación de módulos de Selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+# Importación de otros módulos
+import pyodbc
 import pandas as pd
 from decouple import config
+from datetime import date, timedelta
+import locale
 
 #  ------ Configuración de acceso y conexiona SQL Server ------ 
 conn_str = f'DRIVER={config('driver')};SERVER={config('server')};DATABASE={config('database')};UID={config('username')};PWD={config('password')}'
-print(str(conn_str))
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
-# ------ Fin de configuracion del acceso al servidor ------ 
 
 # ------ Configuracion selenium ------ 
 options = webdriver.ChromeOptions()
 # options.add_argument("--headless")
 service = ChromeService(executable_path="C:/Program Files/Google/Chrome/Application/chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
-# ------ Fin de configuracion selenium ------ 
 
-# ------ Inicio de la navegacion ------ 
-pagina_de_inicio = 'https://www.comercialrefinacion.pemex.com/portal/'
-pagina_volumen_restituido = 'https://www.comercialrefinacion.pemex.com/portal/sccli040/controlador?Destino=sccli040_01.jsp#'
-driver.get(pagina_de_inicio)
-driver.find_element(By.NAME,'usuario').send_keys(config('usuario_Pmx'))
-driver.find_element(By.NAME,'contrasena').send_keys(config('contrasena_pmx'))
-driver.find_element(By.NAME,'botonEntrar').send_keys(Keys.ENTER)
-        
-        #------ En este momento ya hicimos login en la pagina ------ 
+while True:
+    # ------ Inicio de la navegacion ------ 
+    pagina_de_inicio = 'https://www.comercialrefinacion.pemex.com/portal/'
+    pagina_volumen_restituido = 'https://www.comercialrefinacion.pemex.com/portal/sccli040/controlador?Destino=sccli040_01.jsp#'
+    driver.get(pagina_de_inicio)
+    driver.find_element(By.NAME,'usuario').send_keys(config('usuario_Pmx'))
+    driver.find_element(By.NAME,'contrasena').send_keys(config('contrasena_pmx'))
+    driver.find_element(By.NAME,'botonEntrar').send_keys(Keys.ENTER)
+            
+#------ En este momento ya hicimos login en la pagina ------ 
+    try:
+        nombre_user_activo_pmx = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CLASS_NAME, 'textoEjecutivo')))
+        break
+    except: 
+        pass
 
-# Esta parte es opcional 
-wait = WebDriverWait(driver, 5)
-nombre_user_activo_pmx = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'textoEjecutivo')))
-# print(nombre_user_activo_pmx.text)
+#------ Va a el reporte de volumen restituido ------ 
 
-        #------ Va a el reporte de volumen restituido ------ 
-
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+fecha_actual_menos_uno = date.today() - timedelta(days=1)
+mes = fecha_actual_menos_uno.strftime('%B').capitalize()
+dia = int(fecha_actual_menos_uno.day)
 driver.get(pagina_volumen_restituido)
-driver.find_element(By.XPATH,"//input[@value='Consultar']").send_keys(Keys.ENTER)
+ventana_original = driver.current_window_handle
+# Agregar los elementos para cambiar de pagina en el calendario 
+driver.find_element(By.XPATH,"//img[@src='/portal/imagenes/calendario.gif']").click()
+driver.switch_to.window(driver.window_handles[1])
+select_mes = Select(driver.find_element(By.NAME, "selMonth"))
+select_mes.select_by_visible_text(mes)
+driver.find_element(By.LINK_TEXT,str(dia)).click()
+driver.switch_to.window(ventana_original)
+driver.find_element(By.XPATH,"(//img[@src='/portal/imagenes/calendario.gif'])[2]").click()
+driver.switch_to.window(driver.window_handles[1])
+select_mes = Select(driver.find_element(By.NAME, "selMonth"))
+select_mes.select_by_visible_text(mes)
+driver.find_element(By.LINK_TEXT,str(dia)).click()
+driver.switch_to.window(ventana_original)
+driver.find_element(By.XPATH,"//input[@value='Consultar']").click()
 datos_de_tabla_volumenrestituido = driver.find_element(By.XPATH,"//form[@action='/portal/sccli040/controlador']/following-sibling::table[1]")
-# print(datos_de_tabla_volumenrestituido.text)
 
 # Obtener datos de la tabla y dividirlos en filas y columnas
 filas = datos_de_tabla_volumenrestituido.find_elements(By.TAG_NAME, 'tr')
@@ -56,9 +76,27 @@ for fila in filas[1:]:
     fila_data = [celda.text for celda in celdas]
     data.append(fila_data)
 
+
 df = pd.DataFrame(data, columns=[celda.text for celda in columnas])
 # Se descartan las dos ultimas filas ya que no contienen informacion
 df = df.iloc[:-2]
+
+while True:
+    try:
+        # A partir de esta parte se procede con el cierre de sesion
+        driver.find_element(By.XPATH,"//ul[@id='nav']/li[4]/a[1]").click()
+        validacion = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH,"//p[text()=' Contraseña : ']")))
+
+        if validacion.text == 'Contraseña :':
+            print('Cierre de sesión completado con exito')
+            break
+        else:
+            print('No se ha podido cerrar la sesion')
+        break
+    except:
+        pass
+
+driver.quit()
 
 # Se crea una lista con los nombres de los encabezados de las columnas.
 nombres_columnas_df = df.columns.tolist()
@@ -102,9 +140,7 @@ for index, row in df.iterrows():
     cursor.execute(query, tuple(row))
 
 # ------ Elimina los registros duplicados (Si existen) ------ 
-
 query_3 = ("with C as (select  id, comprobante_de_carga, Remision, ROW_NUMBER() over (PARTITION BY comprobante_de_carga ORDER BY comprobante_de_carga desc) AS DUPLICADO FROM Volumen_Restituido) DELETE FROM C WHERE DUPLICADO >1")
-
 cursor.execute(query_3)
 
 # Confirmar los cambios en la base de datos
@@ -112,14 +148,4 @@ conn.commit()
 
 # Cerrar la conexión a SQL Server
 conn.close()
-
-# A partir de esta parte se procede con el cierre de sesion
-driver.find_element(By.XPATH,"//ul[@id='nav']/li[4]/a[1]").click()
-validacion = wait.until(EC.presence_of_element_located((By.XPATH,"//p[text()=' Contraseña : ']")))
-
-if validacion.text == 'Contraseña :':
-    print('Cierre de sesión completado con exito')
-else:
-    print('No se ha podido cerrar la sesion')
-
-driver.quit()
+print('Finalizado')
